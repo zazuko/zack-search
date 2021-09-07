ddde7455d24f54d4e9c1c0901bb1bfb518cb7893
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2021-09-07 14:33:12 +0200

    feat: filter by list

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import './lib/components/zack-type-filters'
 import './lib/components/zack-timeline'
 import './lib/components/zack-filter-summary'
 import './lib/components/zack-query-input'
+import './lib/components/zack-lists'
 
 export { default as rdf } from 'rdf-ext'
 export { default as clownface } from 'clownface'
