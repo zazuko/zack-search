242889c5b4d00fa9a948714f2c4e964533f3b466
tpluscode <tpluscode@users.noreply.github.com>
2021-11-03 10:27:24 +0100

    wip: open result-hierarchy-filter for extensions

--- public/templates/hierarchy.js
+++ public/templates/hierarchy.js
@@ -1,18 +1,32 @@
-export default function ({ html, subject }, { intervalStarts }) {
-  let timeTick = ''
-  if (intervalStarts?.value) {
-    const timelineMargin = 40
-    const date = new Date(intervalStarts.value)
-    const range = this.range.end - this.range.start
-    const width = document.querySelector('zack-timeline').offsetWidth - timelineMargin
-    if (!isNaN(date.valueOf())) {
-      const offset = ((width / range) * (date - this.range.start)) + (timelineMargin / 2)
-      timeTick = html`<zack-result-time-tick .offset="${offset}"></zack-result-time-tick>`
-    }
-  }
+import { dcterms } from 'https://jspm.dev/@tpluscode/rdf-ns-builders'
 
-  return html`
-    ${timeTick}
-    <zack-result-hierarchy .subject="${subject}"></zack-result-hierarchy>
-  `
+export function renderFilter (
+  { operator = '=', prefix = '^', postfix = '+', predicate = dcterms.hasPart, expression } = {}
+) {
+  return ({ html, subject }, { intervalStarts }) => {
+    let timeTick = ''
+    if (intervalStarts?.value) {
+      const timelineMargin = 40
+      const date = new Date(intervalStarts.value)
+      const range = this.range.end - this.range.start
+      const width = document.querySelector('zack-timeline').offsetWidth - timelineMargin
+      if (!isNaN(date.valueOf())) {
+        const offset = ((width / range) * (date - this.range.start)) + (timelineMargin / 2)
+        timeTick = html`
+          <zack-result-time-tick .offset="${offset}"></zack-result-time-tick>`
+      }
+    }
+
+    return html`
+      ${timeTick}
+      <zack-result-hierarchy .subject="${subject}"
+                             .filterPrefix="${prefix}"
+                             .filterPostfix="${postfix}"
+                             .filterOperator="${operator}"
+                             .filterExpression="${expression}"
+      ></zack-result-hierarchy>
+    `
+  }
 }
+
+export default renderFilter()
--- public/templates/tags.js
+++ public/templates/tags.js
@@ -1,11 +1,5 @@
-import { sh } from 'https://jspm.dev/@tpluscode/rdf-ns-builders'
-
 export default function ({ html, shape }, { conceptTags }) {
-  const predicate = shape
-    .out(sh.property)
-    .has(sh.name, 'conceptTags')
-    .out(sh.path)
-    .term
+  const predicate = shape.conceptTags.path.term
 
   return html`<div class="result-tags">
       ${(conceptTags?.terms || []).map(tag => html`
